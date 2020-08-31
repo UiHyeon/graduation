9e5af170cfea88f65aa26d6477d513d860f6aec1
junwon <colin1231@github.com>
2020-08-31 18:38:44 +0900

    수정

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #opencv library
-OPENCVROOT:= C:\Users\user\AndroidStudioProjects\final_project\sdk
+OPENCVROOT:= C:\Users\colin\AndroidStudioProjects\please\sdk
 
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
